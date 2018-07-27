99f5bd6b5483ca82415d43d5a3262bfe49620ce6
Fabian Marin <fabian-guitar@hotmail.com>
2018-07-27 01:39:34 -0500

    download pdf

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, LoadingController } from 'ionic-angular';
+import { File } from '@ionic-native/file';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer';
+import { FileTransfer } from '@ionic-native/file-transfer';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private document: DocumentViewer, private file: File, private transfer: FileTransfer, private platform: Platform, public loadingCtrl: LoadingController) { }
+ 
+ 
+  open() {
+    let loading = this.loadingCtrl.create({
+      content: 'Espere...'
+    });
+  
+    console.log('Mostrar loader');
 
+    loading.present();
+  
+
+    
+    let path = null;
+ 
+    if (this.platform.is('ios')) {
+      path = this.file.documentsDirectory;
+    } else if (this.platform.is('android')) {
+      path = this.file.dataDirectory;
+    }
+ 
+    const transfer = this.transfer.create();
+    transfer.download('http://gahp.net/wp-content/uploads/2017/09/sample.pdf', path + 'nombre-del-archivo.pdf').then(entry => {
+      let url = entry.toURL();
+      console.log('Ocultar loader');
+      loading.dismiss();
+      this.document.viewDocument(url, 'application/pdf', {});
+    });
   }
 
 }
